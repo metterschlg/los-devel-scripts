932aa75a3172aef8eb9e7a841c9871316c0650d9
metterschlg <106471001+metterschlg@users.noreply.github.com>
2022-10-29 13:11:47 +0200

    Simplify build environment setup script.
    As recommended by Ripee in this XDA post:
    https://forum.xda-developers.com/t/unofficial-rom-alpha-lineageos-18-1-for-sm-t715.4509345/post-87620401
    I'm now cloning his build script repo and apply the minor space-saver change I require.

--- build-environment/los-build.sh
+++ build-environment/los-build.sh
@@ -3,57 +3,9 @@ sudo apt install git
 # To debug SELinux (audit2allow tool)
 sudo apt install policycoreutils-python-utils
 
-# Create patch to adjust ripee's build environment script
-echo "Patching ripee's build environment script: "
-echo "  1. Use apt instead of deprecated apt-get"
-echo "  2. Limit compiler cache to 50GB instead of 100GB"
-echo "  3. Clone using https instead of git to not require a github account"
-cat <<EOF > ~/ripee-script.patch
-diff --git a/build_environment.sh b/build_environment.sh
-index e1c9b0c..2cb75f1 100755
---- a/build_environment.sh
-+++ b/build_environment.sh
-@@ -1,15 +1,15 @@
- #!/bin/bash
- 
--sudo apt-get -y update
--sudo apt-get -y upgrade
--sudo apt-get -y dist-upgrade
--sudo apt-get -y remove openjdk-* icedtea-* icedtea6-*
--sudo apt-get -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
-+sudo apt -y update
-+sudo apt -y upgrade
-+sudo apt -y dist-upgrade
-+sudo apt -y remove openjdk-* icedtea-* icedtea6-*
-+sudo apt -y install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libxml2 libxml2-utils lzop openjdk-8-jdk pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
- sudo apt -y autoremove
- mkdir ~/bin
- curl http://commondatastorage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
- chmod a+x ~/bin/repo
--ccache -M 100G
-+ccache -M 50G
- echo 'export ANDROID_COMPILE_WITH_JACK=false' >> ~/.bashrc
- echo 'export CCACHE_EXEC=/usr/bin/ccache' >> ~/.bashrc
- echo 'export LANG=C' >> ~/.bashrc
-diff --git a/lineage-18.1.sh b/lineage-18.1.sh
-index 54add2f..8d4fff5 100755
---- a/lineage-18.1.sh
-+++ b/lineage-18.1.sh
-@@ -2,7 +2,7 @@
- 
- mkdir -p ~/android/lineage-18.1
- cd ~/android/lineage-18.1
--repo init --depth=1 -u git://github.com/LineageOS/android.git -b lineage-18.1
-+repo init --depth=1 -u https://github.com/LineageOS/android.git -b lineage-18.1
- repo sync -c --force-sync --no-clone-bundle --no-tags
- mkdir -p .repo/local_manifests
- cd
-EOF
-git clone https://github.com/ripee/scripts
-cd scripts/
-patch < ~/ripee-script.patch
-
+git clone https://github.com/metterschlg/scripts
 # Setup build environment
+cd scripts
 ./build_environment.sh
 # Initialize LineageOS 18.1 environment
 # As this will also download the sources it can take hours - depending on line speed
