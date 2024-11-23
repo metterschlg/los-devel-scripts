0dce0fef75a59ecadb78e6dd91d0587a35e9b90e
metterschlg <106471001+metterschlg@users.noreply.github.com>
2024-11-23 14:16:06 +0100

    Resolve patch merge conflicts.

--- build-environment/los21ul-fixes.sh
+++ build-environment/los21ul-fixes.sh
@@ -29,9 +29,72 @@ merge_upstream system/core
 
 # October 2024 & November 2024
 merge_upstream device/lineage/sepolicy
+# The fixes to this tree create a merge conflict, resolve manually
+cat <<EOF>>/tmp/device_lineage_sepolicy_mergefix.patch
+--- file_contexts.mergeconflict	2024-11-23 12:54:32.542181252 +0100
++++ file_contexts	2024-11-23 12:54:57.223877846 +0100
+@@ -33,10 +33,7 @@
+ 
+ # Vibrator HAL
+ /(vendor|system/vendor)/bin/hw/android\.hardware\.vibrator@1\.0-service\.lineage u:object_r:hal_vibrator_default_exec:s0
+-<<<<<<< HEAD
++/(vendor|system/vendor)/bin/hw/android\.hardware\.vibrator-service\.legacy       u:object_r:hal_vibrator_default_exec:s0
+ 
+ # Wi-Fi HAL
+ /(vendor|system/vendor)/bin/hw/android\.hardware\.wifi@1\.0-service\.legacy u:object_r:hal_wifi_default_exec:s0
+-=======
+-/(vendor|system/vendor)/bin/hw/android\.hardware\.vibrator-service\.legacy       u:object_r:hal_vibrator_default_exec:s0
+->>>>>>> remotes/upstream/lineage-21.0
+EOF
+cd device/lineage/sepolicy/common/vendor/
+patch -p0 < /tmp/device_lineage_sepolicy_mergefix.patch
+git add file_contexts
+git commit --continue
+rm /tmp/device_lineage_sepolicy_mergefix.patch
+cd $BASEDIR
 
 # October 2024 & November 2024
 merge_upstream vendor/lineage
+# The fixes to this tree create a merge conflict, resolve manually
+cat <<EOF>/tmp/vendor_lineage_mergefix.patch
+--- Android.bp.mergeconflict	2024-11-23 13:57:38.848945499 +0100
++++ Android.bp	2024-11-23 14:11:40.905406782 +0100
+@@ -629,7 +629,6 @@
+ }
+ 
+ soong_config_module_type {
+-<<<<<<< HEAD
+     name: "stagefright_qcom_legacy",
+     module_type: "cc_defaults",
+     config_namespace: "lineageQcomVars",
+@@ -642,7 +641,11 @@
+     soong_config_variables: {
+         uses_qcom_bsp_legacy: {
+             cppflags: ["-DQCOM_BSP_LEGACY"],
+-=======
++        },
++    },
++}
++
++soong_config_module_type {
+     name: "qcom_libfmjni",
+     module_type: "cc_defaults",
+     config_namespace: "lineageQcomVars",
+@@ -655,7 +658,6 @@
+     soong_config_variables: {
+         no_fm_firmware: {
+             cflags: ["-DQCOM_NO_FM_FIRMWARE"],
+->>>>>>> remotes/upstream/lineage-21.0
+         },
+     },
+ }
+EOF
+cd vendor/lineage/build/soong/
+patch -p0 < /tmp/vendor_lineage_mergefix.patch
+git add Android.bp
+git commit --continue
+rm /tmp/vendor_lineage_mergefix.patch
+cd $BASEDIR
 
 # October 2024
 merge_upstream packages/modules/Bluetooth
