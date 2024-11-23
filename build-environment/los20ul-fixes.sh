cb7c5547cec66051b7e41079a63edd41b63bb691
metterschlg <106471001+metterschlg@users.noreply.github.com>
2024-11-23 18:17:53 +0100

    Remove workaround as root cause for RIL breakage got fixed in device tree.
    Signed-off-by: metterschlg <106471001+metterschlg@users.noreply.github.com>

--- build-environment/los20ul-fixes.sh
+++ build-environment/los20ul-fixes.sh
@@ -32,14 +32,3 @@ merge_upstream packages/modules/Wifi
 
 # November 2024
 merge_upstream vendor/lineage
-
-# The following commits as part of the October updates broke gts28ltexx RIL, reverting
-cd ~/android/lineage-20.0/vendor/lineage
-git revert d38b35f32cb38bc67bdb052e37dc6b928198f53c
-git rm overlay/wifionly/packages/apps/Settings/res/values/config.xml
-git revert --continue
-git revert c06535a42bf75f42281502174c6da88d8d5f2dc4
-git rm overlay/wifionly/frameworks/base/core/res/res/values/config.xml
-git revert --continue
-
-cd $BASEDIR
