7f104c0995d4374a8c213c039dc6d6036f2ac0d9
metterschlg <106471001+metterschlg@users.noreply.github.com>
2024-11-30 13:19:37 +0100

    Update patches required to make build work.
    Signed-off-by: metterschlg <106471001+metterschlg@users.noreply.github.com>

--- build-environment/los21ul-fixes.sh
+++ build-environment/los21ul-fixes.sh
@@ -4,7 +4,7 @@
 export BASEDIR=~/android/lineage-21.0/
 
 merge_upstream() {
-  echo "$1"
+  echo "=== $1 ==="
   cd $1
   # As we originally checked out with --depth=1 we need the full trees now to be able to merge
   git fetch --unshallow losul
@@ -49,51 +49,31 @@ EOF
 cd device/lineage/sepolicy/common/vendor/
 patch -p0 < /tmp/device_lineage_sepolicy_mergefix.patch
 git add file_contexts
-git commit --continue
+git merge --continue
 rm /tmp/device_lineage_sepolicy_mergefix.patch
 cd $BASEDIR
 
 # October 2024 & November 2024
 merge_upstream vendor/lineage
-# The fixes to this tree create a merge conflict, resolve manually
-cat <<EOF>/tmp/vendor_lineage_mergefix.patch
---- Android.bp.mergeconflict	2024-11-23 13:57:38.848945499 +0100
-+++ Android.bp	2024-11-23 14:11:40.905406782 +0100
-@@ -629,7 +629,6 @@
+cd vendor/lineage/build/soong/
+cat <<EOF>>/tmp/vendor_lineage_build_soong_Android_bp.patch
+diff --git a/build/soong/Android.bp b/build/soong/Android.bp
+index 1e2832d9..f8bf4429 100644
+--- a/build/soong/Android.bp
++++ b/build/soong/Android.bp
+@@ -642,6 +642,7 @@ stagefright_qcom_legacy {
+         uses_qcom_bsp_legacy: {
+             cppflags: ["-DQCOM_BSP_LEGACY"],
+         },
++    }
  }
  
  soong_config_module_type {
--<<<<<<< HEAD
-     name: "stagefright_qcom_legacy",
-     module_type: "cc_defaults",
-     config_namespace: "lineageQcomVars",
-@@ -642,7 +641,11 @@
-     soong_config_variables: {
-         uses_qcom_bsp_legacy: {
-             cppflags: ["-DQCOM_BSP_LEGACY"],
--=======
-+        },
-+    },
-+}
-+
-+soong_config_module_type {
-     name: "qcom_libfmjni",
-     module_type: "cc_defaults",
-     config_namespace: "lineageQcomVars",
-@@ -655,7 +658,6 @@
-     soong_config_variables: {
-         no_fm_firmware: {
-             cflags: ["-DQCOM_NO_FM_FIRMWARE"],
-->>>>>>> remotes/upstream/lineage-21.0
-         },
-     },
- }
 EOF
-cd vendor/lineage/build/soong/
-patch -p0 < /tmp/vendor_lineage_mergefix.patch
+patch -p0 < /tmp/vendor_lineage_build_soong_Android_bp.patch
 git add Android.bp
-git commit --continue
-rm /tmp/vendor_lineage_mergefix.patch
+git merge --continue
+rm /tmp/vendor_lineage_build_soong_Android_bp.patch
 cd $BASEDIR
 
 # October 2024
@@ -107,3 +87,20 @@ merge_upstream packages/modules/Connectivity
 
 # November 2024
 merge_upstream frameworks/native
+
+# ADB got broken with QPR2, switch back to QPR1
+cat <<EOF>>/tmp/default_xml_adb.patch
+--- default.xml.orig	2024-11-30 12:32:49.227755105 +0100
++++ default.xml	2024-11-30 12:32:44.171653848 +0100
+@@ -1058,7 +1058,7 @@
+   <project path="packages/apps/WallpaperPicker2" name="LineageOS/android_packages_apps_WallpaperPicker2" groups="pdk-fs,sysui-studio" />
+   <project path="packages/inputmethods/LatinIME" name="LineageOS/android_packages_inputmethods_LatinIME" groups="pdk-fs" />
+   <project path="packages/inputmethods/LeanbackIME" name="LineageOS/android_packages_inputmethods_LeanbackIME" groups="pdk-fs" />
+-  <project path="packages/modules/adb" name="LineageOS/android_packages_modules_adb" groups="pdk" />
++  <project path="packages/modules/adb" name="LineageOS-UL/android_packages_modules_adb" remote="losul" revision="lineage-21.0-qpr1" />
+   <project path="packages/modules/AdServices" name="platform/packages/modules/AdServices" groups="pdk-cw-fs,pdk-fs,sysui-studio" remote="aosp" />
+   <project path="packages/modules/AppSearch" name="platform/packages/modules/AppSearch" groups="pdk" remote="aosp" />
+   <project path="packages/modules/ArtPrebuilt" name="platform/packages/modules/ArtPrebuilt" groups="pdk" clone-depth="1" remote="aosp" />
+cd android
+patch -p0 < /tmp/default_xml_adb.patch
+cd $BASEDIR
