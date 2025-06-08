6b0d383583198c374303001b45784ef5aeb0b195
metterschlg <106471001+metterschlg@users.noreply.github.com>
2025-06-08 14:01:18 +0200

    May 2025 los20 security patches.
    Signed-off-by: metterschlg <106471001+metterschlg@users.noreply.github.com>

--- build-environment/los20ul-fixes.sh
+++ build-environment/los20ul-fixes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# April 2025 Android & LineageOS security patches
+# May 2025 Android & LineageOS security patches
 
 export BASEDIR=~/android/lineage-20.0/
 
@@ -21,7 +21,36 @@ merge_upstream() {
 export GIT_EDITOR='true git commit'
 cd $BASEDIR
 
-# April 2025
+# April & May 2025
 merge_upstream frameworks/base
-# April 2025
+# April & May 2025
 merge_upstream packages/modules/Bluetooth
+# May 2025
+merge_upstream packages/modules/Wifi
+
+# April & May 2025 own fork tracking
+cat <<EOF>/tmp/android-fork-tracking.patch
+--- a/default.xml
++++ b/default.xml
+@@ -173,7 +173,7 @@
+   <project path="external/fmtlib" name="platform/external/fmtlib" groups="pdk" remote="aosp" />
+   <project path="external/fonttools" name="platform/external/fonttools" groups="pdk" remote="aosp" />
+   <project path="external/FP16" name="platform/external/FP16" groups="pdk" remote="aosp" />
+-  <project path="external/freetype" name="platform/external/freetype" groups="pdk" remote="aosp" />
++  <project path="external/freetype" name="LineageOS/android_external_freetype" groups="pdk" />
+   <project path="external/fsck_msdos" name="platform/external/fsck_msdos" groups="pdk" remote="aosp" />
+   <project path="external/fsverity-utils" name="platform/external/fsverity-utils" groups="pdk" remote="aosp" />
+   <project path="external/FXdiv" name="platform/external/FXdiv" groups="pdk" remote="aosp" />
+@@ -926,7 +926,7 @@
+   <project path="packages/modules/ExtServices" name="platform/packages/modules/ExtServices" groups="pdk-cw-fs,pdk-fs" remote="aosp" />
+   <project path="packages/modules/GeoTZ" name="platform/packages/modules/GeoTZ" groups="pdk-cw-fs,pdk-fs" remote="aosp" />
+   <project path="packages/modules/Gki" name="platform/packages/modules/Gki" groups="pdk-cw-fs,pdk-fs" remote="aosp" />
+-  <project path="packages/modules/IntentResolver" name="platform/packages/modules/IntentResolver" groups="pdk" remote="aosp" />
++  <project path="packages/modules/IntentResolver" name="LineageOS/android_packages_modules_IntentResolver" groups="pdk" />
+   <project path="packages/modules/IPsec" name="platform/packages/modules/IPsec" groups="pdk" remote="aosp" />
+   <project path="packages/modules/Media" name="platform/packages/modules/Media" groups="pdk" remote="aosp" />
+   <project path="packages/modules/ModuleMetadata" name="platform/packages/modules/ModuleMetadata" groups="pdk" remote="aosp" />
+EOF
+git -C .repo/manifests apply /tmp/android-fork-tracking.patch
+repo sync --force-sync external/freetype
+repo sync --force-sync packages/modules/IntentResolver
