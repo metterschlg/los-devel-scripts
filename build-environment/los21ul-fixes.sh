542fbe8b509aa77e1758bef7753212ee09196551
metterschlg <106471001+metterschlg@users.noreply.github.com>
2025-06-14 15:12:10 +0200

    June 2025 los21 security patches.
    Signed-off-by: metterschlg <106471001+metterschlg@users.noreply.github.com>

--- build-environment/los21ul-fixes.sh
+++ build-environment/los21ul-fixes.sh
@@ -1,6 +1,6 @@
 
 #!/bin/sh
-# May 2025 Android & LineageOS security patches
+# June 2025 Android & LineageOS security patches
 
 export BASEDIR=~/android/lineage-21.0/
 
@@ -22,17 +22,37 @@ merge_upstream() {
 export GIT_EDITOR='true git commit'
 cd $BASEDIR
 
-# April & May 2025
+# April & May & June 2025
 merge_upstream frameworks/base
-
 # April 2025
 merge_upstream frameworks/native
-
-# April & May 2025
+# April & May & June 2025
 merge_upstream packages/modules/Bluetooth
-
 # April 2025
 merge_upstream packages/services/Telephony
+# June 2025
+merge_upstream art
+# June 2025
+merge_upstream frameworks/av
+
+# June 2025 - track LineageOS forks for packages/apps/ManagedProvisioning
+cat <<EOF>/tmp/manifest.patch
+diff --git a/default.xml b/default.xml
+index 4803dd4..4a47c17 100644
+--- a/default.xml
++++ b/default.xml
+@@ -1033,7 +1033,7 @@
+   <project path="packages/apps/HTMLViewer" name="platform/packages/apps/HTMLViewer" groups="pdk-fs" remote="aosp" />
+   <project path="packages/apps/ImsServiceEntitlement" name="platform/packages/apps/ImsServiceEntitlement" groups="pdk-fs" remote="aosp" />
+   <project path="packages/apps/KeyChain" name="platform/packages/apps/KeyChain" groups="pdk-fs" remote="aosp" />
+-  <project path="packages/apps/ManagedProvisioning" name="platform/packages/apps/ManagedProvisioning" groups="pdk-fs" remote="aosp" />
++  <project path="packages/apps/ManagedProvisioning" name="LineageOS/android_packages_apps_ManagedProvisioning" groups="pdk-fs" />
+   <project path="packages/apps/Messaging" name="LineageOS/android_packages_apps_Messaging" groups="pdk-fs" />
+   <project path="packages/apps/Music" name="platform/packages/apps/Music" groups="pdk-fs" remote="aosp" />
+   <project path="packages/apps/MusicFX" name="platform/packages/apps/MusicFX" groups="pdk-fs" remote="aosp" />
+EOF
+git -C .repo/manifests apply /tmp/manifest.patch
+repo sync --force-sync packages/apps/ManagedProvisioning
 
 # Fix ADB Breakage post QPR1 by reverting to QPR1 tree
 echo -e "\n=== packages/modules/adb ==="
