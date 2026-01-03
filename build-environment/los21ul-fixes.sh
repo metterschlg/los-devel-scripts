870c450752ce1956a5d2f4b28f2bf9be6b234a7b
metterschlg <106471001+metterschlg@users.noreply.github.com>
2026-01-03 16:08:03 +0100

    November and December 2025 los21 security patches.
    Signed-off-by: metterschlg <106471001+metterschlg@users.noreply.github.com>

--- build-environment/los21ul-fixes.sh
+++ build-environment/los21ul-fixes.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# September 2025 Android & LineageOS security patches
+# December 2025 Android & LineageOS security patches
 
 export BASEDIR=~/android/lineage-21.0/
 
@@ -21,29 +21,54 @@ merge_upstream() {
 export GIT_EDITOR='true git commit'
 cd $BASEDIR
 
-# April & May & June 2025 & August 2025 & September 2025
+# April & May & June & August & September & December 2025
 merge_upstream frameworks/base
-# April 2025 & September 2025
+# April 2025 & September 2025 & December 2025
 merge_upstream frameworks/native
-# April & May & June 2025 & September 2025
+# September 2025 & December 2025
+merge_upstream frameworks/opt/telephony
+# April & May & June & September & November & December 2025
 merge_upstream packages/modules/Bluetooth
-# April 2025 & September 2025
+# April 2025 & September 2025 & December 2025
 merge_upstream packages/services/Telephony
 # June 2025 & September 2025
 merge_upstream art
 # June 2025 & September 2025
 merge_upstream frameworks/av
-# August 2025 & September 2025
+# August 2025 & September 2025 & December 2025
 merge_upstream vendor/lineage
-# September 2025
-merge_upstream frameworks/opt/telephony
 
-# June 2025 - track LineageOS forks for packages/apps/ManagedProvisioning
-cat <<EOF>/tmp/manifest.patch
+# June 2025 - track LineageOS forks for
+#                 packages/apps/ManagedProvisioning
+# September 2025 - track LineageOS forks for
+#                 packages/modules/CellBroadcastService
+# December 2025 - track LineageOS forks for
+#                 external/sqlite
+#                 packages/apps/CertInstaller
+#                 vendor/apn
+cat <<EOF>/tmp/default-manifest.patch
 diff --git a/default.xml b/default.xml
-index 4803dd4..4a47c17 100644
+index 4803dd4..482917f 100644
 --- a/default.xml
 +++ b/default.xml
+@@ -816,7 +816,7 @@
+   <project path="external/sonic" name="platform/external/sonic" groups="pdk" remote="aosp" />
+   <project path="external/sonivox" name="platform/external/sonivox" groups="pdk" remote="aosp" />
+   <project path="external/speex" name="platform/external/speex" groups="pdk" remote="aosp" />
+-  <project path="external/sqlite" name="platform/external/sqlite" groups="pdk" remote="aosp" />
++  <project path="external/sqlite" name="LineageOS/android_external_sqlite" groups="pdk" />
+   <project path="external/spdx-tools" name="platform/external/spdx-tools" groups="pdk" remote="aosp" />
+   <project path="external/squashfs-tools" name="platform/external/squashfs-tools" groups="pdk" remote="aosp" />
+   <project path="external/stardoc" name="platform/external/stardoc" groups="pdk" remote="aosp" />
+@@ -1025,7 +1025,7 @@
+   <project path="packages/apps/Car/SystemUpdater" name="platform/packages/apps/Car/SystemUpdater" groups="pdk-fs" remote="aosp" />
+   <project path="packages/apps/CarrierConfig" name="platform/packages/apps/CarrierConfig" groups="pdk-cw-fs,pdk-fs" remote="aosp" />
+   <project path="packages/apps/CellBroadcastReceiver" name="LineageOS/android_packages_apps_CellBroadcastReceiver" groups="pdk-cw-fs,pdk-fs" />
+-  <project path="packages/apps/CertInstaller" name="platform/packages/apps/CertInstaller" groups="pdk-cw-fs,pdk-fs" remote="aosp" />
++  <project path="packages/apps/CertInstaller" name="LineageOS/android_packages_apps_CertInstaller" groups="pdk-cw-fs,pdk-fs" />
+   <project path="packages/apps/Contacts" name="LineageOS/android_packages_apps_Contacts" groups="pdk-fs" />
+   <project path="packages/apps/Dialer" name="LineageOS/android_packages_apps_Dialer" groups="pdk-fs" />
+   <project path="packages/apps/DocumentsUI" name="LineageOS/android_packages_apps_DocumentsUI" groups="pdk-cw-fs,pdk-fs" />
 @@ -1033,7 +1033,7 @@
    <project path="packages/apps/HTMLViewer" name="platform/packages/apps/HTMLViewer" groups="pdk-fs" remote="aosp" />
    <project path="packages/apps/ImsServiceEntitlement" name="platform/packages/apps/ImsServiceEntitlement" groups="pdk-fs" remote="aosp" />
@@ -53,16 +78,6 @@ index 4803dd4..4a47c17 100644
    <project path="packages/apps/Messaging" name="LineageOS/android_packages_apps_Messaging" groups="pdk-fs" />
    <project path="packages/apps/Music" name="platform/packages/apps/Music" groups="pdk-fs" remote="aosp" />
    <project path="packages/apps/MusicFX" name="platform/packages/apps/MusicFX" groups="pdk-fs" remote="aosp" />
-EOF
-git -C .repo/manifests apply /tmp/manifest.patch
-repo sync --force-sync packages/apps/ManagedProvisioning
-
-# September 2025 - track LineageOS forks for packages/modules/CellBroadcastService
-cat <<EOF>/tmp/manifest.patch
-diff --git a/default.xml b/default.xml
-index 4803dd4..89e7157 100644
---- a/default.xml
-+++ b/default.xml
 @@ -1070,7 +1070,7 @@
    <project path="packages/modules/ArtPrebuilt" name="platform/packages/modules/ArtPrebuilt" groups="pdk" clone-depth="1" remote="aosp" />
    <!--<project path="packages/modules/Bluetooth" name="LineageOS/android_packages_modules_Bluetooth" groups="pdk" />-->
@@ -73,8 +88,27 @@ index 4803dd4..89e7157 100644
    <project path="packages/modules/ConfigInfrastructure" name="platform/packages/modules/ConfigInfrastructure" groups="pdk-cw-fs,pdk-fs" remote="aosp" />
    <!--<project path="packages/modules/Connectivity" name="LineageOS/android_packages_modules_Connectivity" groups="pdk-cw-fs,pdk-fs" />-->
 EOF
-git -C .repo/manifests apply /tmp/manifest.patch
+cat <<EOF>/tmp/lineage-manifest.patch
+diff --git a/snippets/lineage.xml b/snippets/lineage.xml
+index 05142b8..251527c 100644
+--- a/snippets/lineage.xml
++++ b/snippets/lineage.xml
+@@ -55,6 +55,7 @@
+   <project path="packages/resources/devicesettings" name="LineageOS/android_packages_resources_devicesettings" />
+   <project path="prebuilts/extract-tools" name="LineageOS/android_prebuilts_extract-tools" clone-depth="1" />
+   <project path="tools/extract-utils" name="LineageOS/android_tools_extract-utils" />
++  <project path="vendor/apn" name="LineageOS/android_vendor_apn" revision="main" />
+   <project path="vendor/crowdin" name="LineageOS/android_vendor_crowdin" />
+   <!--<project path="vendor/lineage" name="LineageOS/android_vendor_lineage" />-->
+ 
+EOF
+git -C .repo/manifests apply /tmp/default-manifest.patch
+git -C .repo/manifests apply /tmp/lineage-manifest.patch
+repo sync --force-sync packages/apps/ManagedProvisioning
 repo sync --force-sync packages/modules/CellBroadcastService
+repo sync --force-sync external/sqlite
+repo sync --force-sync packages/apps/CertInstaller
+repo sync --force-sync vendor/apn
 
 # Fix ADB Breakage post QPR1 by reverting to QPR1 tree
 echo -e "\n=== packages/modules/adb ==="
